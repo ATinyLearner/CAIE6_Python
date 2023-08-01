e3ae4bd682b3c34922c59b68c47162a78e895ca7
Avik Ghosh <36378913+ATinyLearner@users.noreply.github.com>
2023-08-01 15:38:00 +0530

    Python 01.

---
+++ Python_01.08.23.py
@@ -0,0 +1,24 @@
+# python data types
+int #12 13 1 2
+str # ayush,avik,
+float # 12.23,5.69
+
+
+# print(18/5)
+# print(18//5)
+# print(18%5)
+
+n=int(input("Enter a number:")) #variable n
+if(n % 2 == 1): #checking odd
+        print("Weird")
+else:
+    if(n>2 and n<5):
+        print("Not Weird")
+    elif(n>6 and n<=20):
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+
+
